4073d85da5ac7debd9c064a1cfd7e610945e28db
Aman <jhurani2@illinois.edu>
2026-04-15 11:19:06 -0500

    Handle None IP address for devices that are not present.

--- utilities/get_IP.py
+++ utilities/get_IP.py
@@ -2,14 +2,16 @@ import os
 # Following https://stackoverflow.com/a/59559717
 
 def get_IP_from_MAC(MAC):
-    for device in os.popen('arp -a'):
-        # example output: xxxx (192.168.1.254) at xx:xx:xx:xx:xx:xx [ether] on wlp..
-        _, ip, _, phy, _ = device.split(maxsplit=4)
-        # remove the paranthesis around the ip address
-        ip = ip.strip('()')
+    if MAC is not None:
+        for device in os.popen('arp -a'):
+            # example output: xxxx (192.168.1.254) at xx:xx:xx:xx:xx:xx [ether] on wlp..
+            _, ip, _, phy, _ = device.split(maxsplit=4)
+            # remove the paranthesis around the ip address
+            ip = ip.strip('()')
 
-        if phy == MAC:
-            return ip
+            if phy == MAC:
+                return ip
 
-    print('Could not find %s' % MAC)
+        print('Could not find %s' % MAC)
+    
     return None
